fe6632c2290e5bf861a8fd88c88bc158143f626c
Kirby Ezekiel <kirby.santos@obf.ateneo.edu>
2020-03-18 15:18:41 +0800

    Added test for checking if the ingredients list page returns the correct template

--- froyo/tests.py
+++ froyo/tests.py
@@ -5,3 +5,8 @@ class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
         response = self.client.get('')
         self.assertTemplateUsed(response, 'home_page.html')
+
+class IngredientsListPageTest(TestCase):
+    def test_ingredients_list_page_returns_correct_html(self):
+        response = self.client.get('/ingredients-list')
+        self.assertTemplateUsed(response, 'ingredients_list.html')
