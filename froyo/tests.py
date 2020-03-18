4fc36640cd3c034c6408b55598798761bc0a0ddf
Kirby Ezekiel <kirby.santos@obf.ateneo.edu>
2020-03-19 02:55:00 +0800

    Added a test for checking if recipes list contains css

--- froyo/tests.py
+++ froyo/tests.py
@@ -96,3 +96,9 @@ class PageContainsCssTest(TestCase):
         html = response.content.decode('utf8')
         self.assertIn('<link rel="stylesheet" href="/static/bootstrap.min.css">', html)
         self.assertIn('<link rel="stylesheet" href="/static/styles.css">', html)
+
+    def test_recipes_list_contains_css(self):
+        response = self.client.get('/recipes_list')
+        html = response.content.decode('utf8')
+        self.assertIn('<link rel="stylesheet" href="/static/bootstrap.min.css">', html)
+        self.assertIn('<link rel="stylesheet" href="/static/styles.css">', html)
