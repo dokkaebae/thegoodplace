4271505e878c79135f28d974bbecc70ff7e67786
Kirby Ezekiel <kirby.santos@obf.ateneo.edu>
2020-03-19 03:36:55 +0800

    Added a test for checking if orders list contains css

--- froyo/tests.py
+++ froyo/tests.py
@@ -126,3 +126,9 @@ class PageContainsCssTest(TestCase):
         html = response.content.decode('utf8')
         self.assertIn('<link rel="stylesheet" href="/static/bootstrap.min.css">', html)
         self.assertIn('<link rel="stylesheet" href="/static/styles.css">', html)
+
+    def test_orders_detail_contains_css(self):
+        response = self.client.get('/orders-detail')
+        html = response.content.decode('utf8')
+        self.assertIn('<link rel="stylesheet" href="/static/bootstrap.min.css">', html)
+        self.assertIn('<link rel="stylesheet" href="/static/styles.css">', html)
